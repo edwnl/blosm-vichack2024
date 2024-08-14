60f452f8254bba73c323572c117022f33339af80
ed <edwinli123456@gmail.com>
2024-08-14 19:34:17 +1000

    installed and configured nativewind v4, tailwind and prettier

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text className={"text-5xl text-blue-500 text-center"}>
+        If you see some large blue text, tailwind is working!
+      </Text>
     </View>
   );
 }
